4dba5a62d7015c33ed8152c8b19d52b97978d344
Riley Griffin <rigriffin241@gmail.com>
2018-10-20 13:16:57 -0400

    made branch with changes

--- routes/routes.js
+++ routes/routes.js
@@ -2,13 +2,12 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const controllers = require('../controllers/api');
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
-const parseJson = bodyParser.json();
 const { validateBody, schemas } = require('../helpers/validate');
 
 module.exports = {
     api: async(app)=>{
-        app.use('/api', urlencodedParser, parseJson, controllers.api);
+        app.use(bodyParser.json());
+        app.get('/api', controllers.api);
         app.post('/api', validateBody(schemas.ast), controllers.update);
     }
 };
--- server.js
+++ server.js
@@ -24,7 +24,6 @@ db.on("error", console.error.bind(console, "connection error:"));
 
 db.once('open', function() {
     routes.api(app);
-
     app.listen(PORT);
     console.log('app listening...');
 

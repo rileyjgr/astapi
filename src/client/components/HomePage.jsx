d6163c37e8261f3ed3d86c1359558ceb7de0546c
Riley Griffin <rigriffin241@gmail.com>
2018-10-21 00:44:44 -0400

    made some format changes

--- src/client/components/HomePage.jsx
+++ src/client/components/HomePage.jsx
@@ -35,19 +35,23 @@ class HomePage extends Component {
         const richterScale = state.richterScale;
         return (
         <div>
-            <div id="knowledge">
-                <h4> Example Queries to get /api/:name example name, {name}.
-                    <p>You will received a response as a json object as such:</p>
-                    <ul> {name}:
-                        <p>Energy: {energy}</p>
-                        <p>Mass: {mass}</p>
-                        <p>Velocity: {vel}</p>
-                        <p>BlastRadius: {blastRadius}</p>
-                        <p>Fireball: {fireball}</p>
-                        <p>Richter Scale: {richterScale}</p>
-                    </ul>
-                </h4>
-            </div>
+            <header className="masthead">
+                <div className="container d-flex h-100 align-items-center">
+                    <div className="mx-auto text-center">
+                        <h1 className="mx-auto my-0 text-uppercase">Example Queries to get /api/:name example name, {name}.</h1>
+                        <h2 className="mx-auto my-0">You will received a response as a json object as such:</h2>
+                        <ul className="mx-auto my-0">Name: {name}
+                            <p className="mx-auto my-0">Energy: {energy}</p>
+                            <p className="mx-auto my-0">Mass: {mass}</p>
+                            <p className="mx-auto my-0">Velocity: {vel} </p>
+                            <p className="mx-auto my-0">BlastRadius: {blastRadius}</p>
+                            <p className="mx-auto my-0">Fireball: {fireball}</p>
+                            <p className="mx-auto my-0">Richter Scale: {richterScale}</p>
+                        </ul>
+                        <a href="" className="btn btn-primary js-scroll-trigger">Jump into the docs!</a>
+                    </div>
+                </div>
+            </header>
             <div id="links">
                 <a href="https://teasteroidm-api.herokuapp.com/api">"Full" - Api.</a>
                 <br/>
